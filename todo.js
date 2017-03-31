41d541647f27a7bcc43deacaaf2a6d19bfc8906c
Ali Fatahi <a.fatahi1992@gmail.com>
2017-03-31 22:52:05 +0430

    Create todo.Js file to do all Java Script Code in There

---
+++ todo.js
@@ -0,0 +1,12 @@
+/**
+ * Created by AI on 3/31/2017.
+ */
+//        <li><input type="checkbox"><span>Write My Wish</span></li>
+
+    function addNewItem() {
+
+    }
+var btnNew = document.getElementById('btnAdd');
+
+btnNew.onclick = addNewItem;
+
