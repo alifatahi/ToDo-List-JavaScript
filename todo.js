89b0e3e0f7c45dbdf21e3157aa1b1a152f76b446
Ali Fatahi <a.fatahi1992@gmail.com>
2017-04-01 11:44:39 +0430

    Comment Some Code

--- todo.js
+++ todo.js
@@ -3,15 +3,27 @@
  */
 //        <li><input type="checkbox"><span>Write My Wish</span></li>
 
+    /**
+    * Function for add New Items
+     * reason we use argument is because
+     * we can use it with another Id
+    * @param list
+    */
     function addNewItem(list) {
+       //using create element with li to make our list
         var listItem = document.createElement("li");
-
+        //put Text on our list
         listItem.innerText= "Hello";
+        //append text and list to our function argue
         list.appendChild(listItem);
     }
-var btnNew = document.getElementById("btnAdd");
-//we should not give our addNewItem function () because its not call onclick
+    //this var take ID From Our Form button
+    var btnNew = document.getElementById("btnAdd");
+/**
+ * using onclick function to when user Click
+ * run addNewItem function and pass our list Id
+ */
 btnNew.onclick = function () {
     addNewItem(document.getElementById("todoList"));
-};
+    };
 
